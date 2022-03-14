a29b3b2c38512ce9ed9e8815aa3674e53643d8dc
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-14 13:39:45 +0700

    Menyelesaikan tugas 1

---
+++ task1.js
@@ -0,0 +1,35 @@
+const cekHariKerja = (day) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dataDay = ["senin", "selasa", "rabu", "kamis", "jumat"];
+      let cek = dataDay.find((item) => {
+        return item.toLowerCase() === day.toLowerCase();
+      });
+
+      if (cek) {
+        resolve(cek);
+      } else {
+        reject(new Error("Hari ini bukan hari kerja"));
+      }
+    }, 3000);
+  });
+};
+
+// then-catch
+cekHariKerja("rAbu")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+// async-await
+const cekHariKerjaAsync = async (day) => {
+  try {
+    console.log(await cekHariKerja(day));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+cekHariKerjaAsync("minggu");
